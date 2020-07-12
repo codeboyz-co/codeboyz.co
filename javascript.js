82613cb691965dddcd8b4ae5817c45df4984d52a
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 19:36:02 +0200

    made some changes again

--- javascript.js
+++ javascript.js
@@ -12,10 +12,10 @@ function hideForm() {
     const form1 = document.querySelector('form')
     if(!form1) {
         return;
-    }
+    };
     else {
     form1.style.display = form1.style.display === 'none' ? 'block' : 'none';
     alert("Bedankt voor het invullen! Uw gegevens worden")
     document.getElementById("button").style.display = 'none'
-    }
+    };
 }
