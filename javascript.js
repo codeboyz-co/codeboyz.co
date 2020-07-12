68dbc4fedec9692140d68bdb7f4ab09d9167d7a2
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 14:07:17 +0200

    even more changes

--- javascript.js
+++ javascript.js
@@ -7,10 +7,10 @@ function showForm() {
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 }
 function hideForm() {
-    const form = document.querySelector('form')
+    
     if(!form) {
         return;
     }
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
-    
+    return <p>Bedankt voor het invullen! Uw gegevens worden verwerkt.</p>
 }
