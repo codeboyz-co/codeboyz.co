0de94fca080152e6f4ccf90662d29e0049c366ba
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 13:17:37 +0200

    added more code for the 'click' button

--- javascript.js
+++ javascript.js
@@ -6,3 +6,11 @@ function showForm() {
 
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 }
+function hideForm() {
+    const form1 = document.querySelector('form')
+    if(!form) {
+        return;
+    }
+    form.style.display = form.style.display === 'block' ? 'none' : 'block';
+    console.log("Bedankt voor het invullen! Uw gegevens worden verwerkt.")
+}
