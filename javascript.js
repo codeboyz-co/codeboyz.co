b4741fe01f6e82aa4b8a6d1161d802ddab6de6fa
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 14:01:06 +0200

    even more changes

--- javascript.js
+++ javascript.js
@@ -12,5 +12,5 @@ function hideForm() {
         return;
     }
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
-     return "Bedankt voor het invullen! Uw gegevens worden verwerkt."
+     return <h1>Bedankt voor het invullen! Uw gegevens worden verwerkt."</h1>
 }
