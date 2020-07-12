ef0f17201f6a2f8862eb5ff50ca70cb02e2406fb
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 19:32:45 +0200

    changed some things back

--- javascript.js
+++ javascript.js
@@ -9,15 +9,13 @@ function showForm() {
     }
 }
 function hideForm() {
-    let hoi = document.getElementById("invultekst").innerHTML
     const form1 = document.querySelector('form')
     if(!form1) {
         return;
     }
     else {
     form1.style.display = form1.style.display === 'none' ? 'block' : 'none';
-    hoi = "Bedankt voor het invullen!"
-    document.getElementById('button').style.display = 'none' 
     alert("Bedankt voor het invullen! Uw gegevens worden")
+    document.getElementById("button").style.display = 'none'
     }
 }
