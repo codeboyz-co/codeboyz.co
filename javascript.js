0e2e415a03c471c50f13df4645cde565bc9b3ce7
Obadja Ris <obadjaris@obadja-mbp.lan>
2020-07-12 18:56:05 +0200

    added an alert

--- javascript.js
+++ javascript.js
@@ -18,4 +18,5 @@ function hideForm() {
     form1.style.display = form1.style.display === 'none' ? 'block' : 'none';
     hoi = "Bedankt voor het invullen!"
     document.getElementById('button').style.display = 'none'
+    alert("Bedankt voor het invullen!")
 }
