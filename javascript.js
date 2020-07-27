5b539f09858f9435b6b801c42d40c864f073c0cc
Obadja Ris <obadjaris@obadja-mbp.local>
2020-07-27 16:49:45 +0200

    changed more things

--- javascript.js
+++ javascript.js
@@ -3,17 +3,10 @@ class Contact {
   constructor () {
     this.form = document.querySelector('form')
     this.form.addEventListener('submit', () => this.submitForm());
-
-    this.toggleButton = document.querySelector('button.toggle');
-    this.toggleButton.addEventListener('click', () => this.toggleForm());
-  }
-
-  toggleForm () {
-    this.form.classList.toggle('visible');
   }
 
   submitForm () {
     alert('Bedankt voor het invullen! Uw gegevens worden verwerkt');
-    this.toggleForm();
+    
   }
 }
